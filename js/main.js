ae32f600505dccd235072ff642cb10ae7d6ab2c1
Peam <ptmatanui@gmail.com>
2017-11-20 23:50:53 +0700

    Updated11-20-2017-2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 	function preload() {
-		game.load.image('background', 'assets/images/rsz_1background.png');
+		game.load.image('background', 'assets/images/background1.png');
 		game.load.image('wizard','assets/images/wizard1.png');
 		game.load.image('fireball','assets/images/fireball.png');
 		game.load.image('monster1','assets/images/mon1.png');
+		game.load.image('wall','assets/images/wall.png');
 	}
 
 	var sprite;
 	var fireballs;
 	var monsters;
+	var wall;
 
 	var fireRate = 500;
 	var nextFire = 0;
@@ -16,9 +18,25 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create
 	var score = 0;
 	var killed = 0;
 	var scoreTime =0;
+	var health = 5;
+	var temp;
 
 	function create() {
 		game.add.sprite(0, 0, 'background');
+
+		walls = game.add.group();
+		walls.enableBody = true;
+		walls.physicsBodyType = Phaser.Physics.ARCADE;
+
+		walls.createMultiple(6,'wall');
+		walls.setAll('checkWorldBounds',true);
+
+		wall = walls.getFirstExists(false);
+		wall.reset(300 , 745);
+		wall.anchor.set(0.5);
+		wall.scale.x = 0.25;
+		wall.scale.y = 0.22;
+		
 
 		fireballs = game.add.group();
     	fireballs.enableBody = true;
@@ -28,7 +46,7 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create
     	fireballs.setAll('checkWorldBounds', true);
     	fireballs.setAll('outOfBoundsKill', true);
 
-    	sprite = game.add.sprite(300, 700, 'wizard');
+    	sprite = game.add.sprite(300, 720, 'wizard');
 		sprite.anchor.set(0.5);
 		sprite.scale.x = 0.4;
 		sprite.scale.y = 0.4;
@@ -46,7 +64,8 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create
 	}
 	function update() {
 		game.physics.arcade.overlap(fireballs, monsters, collisionHandler, null, this);
-		sprite.rotation = game.physics.arcade.angleToPointer(sprite) + Math.PI/2;
+		game.physics.arcade.collide(wall, monsters, collisionHandler2, null, this);
+		sprite.rotation = game.physics.arcade.angleToPointer(sprite);
        	fire();
        	spawn();
        	if(game.time.now>scoreTime){
@@ -55,6 +74,7 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create
        	}
 		game.debug.text('Score : '+score,450,32);
 		game.debug.text('killed : '+killed,450,64);
+		game.debug.text('health : '+health,450,92);
 	}
 	function fire() {
 	    if (game.time.now > nextFire && fireballs.countDead() > 0)
@@ -66,7 +86,7 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create
 	        fireball.scale.x = 0.05;
 			fireball.scale.y = 0.05;
 
-	        fireball.reset(sprite.x - 29, sprite.y-30);
+	        fireball.reset(sprite.x - 30, sprite.y-30);
 
 	        game.physics.arcade.moveToPointer(fireball, 300);
 	    }
@@ -77,6 +97,17 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create
 	    fireball.kill();
 	    monster.kill();
 	    killed+=1;
+
+	}
+
+	function collisionHandler2 (wall, monster) {
+
+	    monster.kill();
+	    wall.reset(300 , 745);
+		wall.anchor.set(0.5);
+		wall.scale.x = 0.25;
+		wall.scale.y = 0.22;
+	    health-=1;
 
 	}
 
@@ -92,4 +123,9 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, '', { preload: preload, create
 				spawnTime = game.time.now +1500;
 			}
 		}
+	}
+	function processHandler (wall,monster) {
+
+	    return true;
+
 	}
